11353c921aa781b89350d3ca46fb49bf2bca8678
suecmorin <suecmorin@gmail.com>
2022-11-29 06:47:39 -0700

    worked on caching

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Text Editor Starter Code
+## PROJECT NAME Text Editor 
+
+## DESCRIPTION  Text editor using indexed Data Base (idb) API, a Progressive Web App (PWA). App is deployed to Heroku. Video of functionality is available here: xx.
+
+## INSTALLATION   
+
+## CONTRIBUTING
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -10,21 +10,50 @@ module.exports = () => {
   return {
     mode: 'development',
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: './assets/js/index.js',
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
-    plugins: [
-      
-    ],
-
-    module: {
-      rules: [
-        
-      ],
+    {
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
     },
-  };
-};
+    {
+      test: /\.m?js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+          plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+        },
+      },
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Text editor'
+      }),
+      new GenerateSW(),
+      new WebpackPwaManifest({
+        name: 'Text editor',
+        short_name: 'Text editor',
+        description: 'Keep track of important tasks!',
+        background_color: '#7eb4e2',
+        theme_color: '#7eb4e2',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/favicon.ico'),
+            sizes: [96, 128, 192, 256, 384, 512],
+          },
+        ],
+      }),
+    ],
+  },
+}
+
+
