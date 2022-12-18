a201c959579a2165f323894571ab60f2b07718ae
suecmorin <suecmorin@gmail.com>
2022-12-18 16:31:40 -0700

    service worker

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 ## PROJECT NAME Text Editor 
 
 ## DESCRIPTION  
-Text editor using indexed Data Base (idb) API, a Progressive Web App (PWA). App uses webpack,  manifest and service worker to generate HTML file. App is deployed to Heroku. Video of functionality is available here: xx.
+Text editor using indexed Data Base (idb) API, a Progressive Web App (PWA). App uses webpack,  manifest and service worker to generate HTML file. App is deployed to Heroku. 
+! [screenshot] ()https://github.com/suecmorin/text-editor/screenshot.png)
+Video of functionality is available here: xx.
 
 ## INSTALLATION   
-
+Clone the repository; type "npm install concurrently" to install client and server necessities, then type "npm run start:dev".
 ## CONTACT ME
 Contact me at suecmorin@gmail.com.
--- client/src-sw.js
+++ client/src-sw.js
@@ -1,6 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
-const { StaleWhileRevalidate } = require('workbox-strategies');
+const { CacheFirst,  StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -38,47 +37,10 @@ registerRoute(
       new CacheableResponsePlugin({
         statuses: [0, 200],
       }),
-    ],
-  })
-);
-
-
-
-
-
-
-
-
-
-warmStrategyCache({
-  urls: ['/index.html', '/'],
-  strategy: pageCache,
-});
-
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
-
-// Implement asset caching--credit Chrome Developers "caching resources during runtime"
-const scriptsRoute = new Route(({ request }) => {
-  return request.destination === 'script';
-}, new CacheFirst ({
-     cacheName: 'scripts',
-     plugins: [
-        new ExpirationPlugin({
-             maxEntries: 50,
-        })
-     ]
-}));
-
-const stylesRoute= new Route(({ request }) => {
-  return request.destination === 'style';
-}, new CacheFirst ({
-     cacheName: 'styles',
-     plugins: [
       new ExpirationPlugin({
-          maxAgeSeconds: 60 * 60 * 24 * 30,
-      })
-     ]
-}));
-registerRoute(scriptsRoute);
-registerRoute(stylesRoute);
-
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60
+      }),
+    ],
+  }),
+);
